d79f820901cce0b3c8206b1b88b570de19916667
ezfuzzy <mentor787@naver.com>
2024-06-24 18:20:53 +0900

    add: 24/06/24

---
+++ yrdy/01/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MemorySimulator from "./components/MemorySimulator";
+
+function App() {
+  return (
+    <div className="App">
+      <MemorySimulator />
+    </div>
+  );
+}
+
+export default App;
