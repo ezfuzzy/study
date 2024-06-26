79b88bc2d8e1eefcaa496379ee460946f58a6f0b
ezfuzzy <ezfuzzy062@gmail.com>
2024-06-26 21:13:46 +0900

    vv

--- yrdy/02/client/src/App.js
+++ yrdy/02/client/src/App.js
@@ -1,16 +1,23 @@
-import React from "react";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import Chat from "./components/Chat";
-import Join from "./components/join";
+import React, { useState } from 'react';
+import ChatPopup from './ChatPopup';
 
 function App() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const toggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Join />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </Router>
+    <div className="relative">
+      <button
+        onClick={toggleChat}
+        className="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full shadow-lg"
+      >
+        Chat
+      </button>
+      {isChatOpen && <ChatPopup onClose={toggleChat} />}
+    </div>
   );
 }
 
