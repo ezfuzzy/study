6ad4b4a4061c584cc08ee09f208b9565e16ca807
ezfuzzy <mentor787@naver.com>
2024-07-16 11:40:06 +0900

    add: rcc

--- react_test/base/src/App.js
+++ react_test/base/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  // state definition
+  state = {
+    count: 0,
+  };
+
   // rcc => class형 component : Component를 상속받아서 만듬
   // rcs => function형 Component : 함수형임
   render() {
     return (
       <div>
         <h1>index page</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}>
+          {this.state.count}
+        </button>
       </div>
     );
   }
