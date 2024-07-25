5918c90189fb168eadc8a4a6b36e6721a7eb3312
ezfuzzy <mentor787@naver.com>
2024-07-25 18:01:18 +0900

    add: useState

--- react_test/base/src/App.js
+++ react_test/base/src/App.js
@@ -1,31 +1,138 @@
-import React, { Component } from "react";
-import { NavLink, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css"
-import { Home, NotFound, Play, Post, Study } from "./pages";
+// App.css 적용하기 (내부 css)
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
+//함수형 component
+function App() {
+  // profile
+  const [profile, setProfile] = useState({
+    idx: 1,
+    name: "ezfz",
+    game: "lol",
+  });
+  const [newKey, setNewKey] = useState("");
+  const [newValue, setNewValue] = useState("");
+  // array
+  const [newName, setNewName] = useState("");
+  const [names, setNames] = useState(["ezfz", "hysz", "lessa"]);
+  // boolean
+  const [isShow, setShow] = useState(true);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>index page</h1>
-        <h3>----------</h3>
-        <ul className="nav nav-pills">
-          <li className="nav-item"><NavLink className="nav-link" to="/">Home</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link" to="/play">Play</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link" to="/study">Study</NavLink></li>
-          <li className="nav-item"><NavLink className="nav-link" to="/post">Post</NavLink></li>
-        </ul>
-        <Routes>
-          <Route path="/" Component={Home} />
-          <Route path="/play" Component={Play} />
-          <Route path="/study/*" Component={Study} />
-          <Route path="/post" Component={Post} />
-          <Route path="/*" Component={NotFound} />
-        </Routes>
+  const [nameList, setNameList] = useState({
+    inputName: "",
+    names: [],
+    seq: 0,
+  });
+
+  console.log("mounted");
+
+  const handleAddProperty = () => {
+    setProfile((prevProfile) => ({
+      ...prevProfile,
+      [newKey]: newValue,
+    }));
+    setNewKey("");
+    setNewValue("");
+  };
+
+  const handleIncrementIdx = () => {
+    setProfile((prevProfile) => ({
+      ...prevProfile,
+      idx: prevProfile.idx + 1,
+    }));
+  };
+
+  const handleAddName = () => {
+    setNames([...names, newName]);
+    setNewName("");
+  };
+
+  const handleShow = (e) => {
+    setShow(e.target.checked);
+  };
+  
+  const replacer = (key, value) => {
+    if (key === 'inputName') {
+      return undefined;
+    }
+    return value;
+  };
+
+  return (
+    <div className="container">
+      <h1>인덱스 페이지 입니다</h1>
+      {/* profile */}
+      <div style={{ padding: "20px", border: "solid 2px #062" }}>
+        <h1>프로필 정보</h1>
+        {Object.keys(profile).map((key) => (
+          <p key={key}>
+            <strong>{key}:</strong> {profile[key]}
+            {key === "idx" && (
+              <button className="btn btn-success btn-sm m-3" onClick={handleIncrementIdx}>
+                +
+              </button>
+            )}
+          </p>
+        ))}
+        <input
+          className="form-control my-3"
+          type="text"
+          placeholder="Key"
+          value={newKey}
+          onChange={(e) => setNewKey(e.target.value)}
+        />
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Value"
+          value={newValue}
+          onChange={(e) => setNewValue(e.target.value)}
+        />
+        <button className="btn btn-primary mt-3" onClick={handleAddProperty}>
+          Add Property
+        </button>
       </div>
-    );
-  }
+      {/* array */}
+      <div className="mt-3">
+        <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+        <button className="btn btn-info " onClick={handleAddName}>
+          add
+        </button>
+        <ul>
+          {names.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {/* boolean, undefined, null */}
+        <input type="checkbox" checked={isShow} onChange={handleShow} />
+        {isShow && (
+          <p>
+            true: {true} <br />
+            undefined: {undefined} <br />
+            null: {null}
+          </p>
+        )}
+        <div className="mt-3">
+          <input type="text" onChange={(e) => {setNameList({ ...nameList, inputName: e.target.value })}} />
+          <button onClick={() => {setNameList({
+            ...nameList,
+            names: [...nameList.names, { name: nameList.inputName, id: nameList.seq }],
+            seq: nameList.seq+1
+          })}}>add</button>
+          <ul>
+            {nameList.names.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+          <pre>{JSON.stringify(nameList, replacer, 2)}</pre>
+        </div>
+      </div>
+    </div>
+  );
 }
 
+//외부에서 App.js 를 import 하면 App 함수를 사용할수 있다. (src/index.js)
 export default App;
