48c83ba6f018c8dce760068a7f814999239bf32f
ezfuzzy <mentor787@naver.com>
2024-08-23 11:36:53 +0900

    add: 인증번호 test

--- react_test/base/src/pages/tripDuo/THome.js
+++ react_test/base/src/pages/tripDuo/THome.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { decodeToken } from "jsontokens";
 import React, { useRef, useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 function THome(props) {
-  const username = useRef();
-  const password = useRef();
-
+  const inputPhoneNum = useRef();
+  const inputVerificationCode = useRef();
   const [userData, setUserData] = useState({
     username: "",
     password: "",
@@ -16,23 +16,18 @@ function THome(props) {
       ...userData,
       [e.target.name]: e.target.value,
     });
-
-    console.log(userData);
-    
   };
 
   const handleLogin = () => {
     axios
       .post("/api/v1/auth/login", userData)
       .then((res) => {
-
         console.log(res.data);
         localStorage.tripDuo_token = res.data;
         const result = decodeToken(localStorage.tripDuo_token.substring(7));
         const username = result.payload.sub;
         axios.defaults.headers.common["Authorization"] = localStorage.tripDuo_token;
         console.log(username);
-        
       })
       .catch((error) => {
         console.log(error);
@@ -43,22 +38,130 @@ function THome(props) {
     axios
       .post("/api/v1/auth/signup", userData)
       .then((res) => {
-        
         console.log(res.data);
-        
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
+
+  const handle인증번호받기 = () => {
+    const phoneNumber = inputPhoneNum.current.value;
+
+    axios
+      .post("/api/v1/auth/sms", phoneNumber)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handle인증번호확인 = () => {
+    const phoneNum = inputPhoneNum.current.value;
+    const code = inputVerificationCode.current.value;
+    console.log(code);
+
+    axios
+      .post("/api/v1/auth/sms/verify", phoneNum.substring(0, phoneNum.length) + code)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handle인증번호받기2 = () => {
+    const phoneNumber = inputPhoneNum.current.value;
+
+    console.log(typeof phoneNumber);
+    console.log(phoneNumber);
+
+    axios
+      .post("/api/v1/auth/send", { phoneNumber: phoneNumber })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handle인증번호확인2 = () => {
+    const phoneNum = inputPhoneNum.current.value;
+    const code = inputVerificationCode.current.value;
+    console.log(code);
+
+    axios
+      .post("/api/v1/auth/verify", phoneNum+code)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
-    <div>
-      <input ref={username} name="username" type="text" onChange={handleChange} />
-      <input ref={password} name="password" type="text" onChange={handleChange} />
-      <button onClick={handleLogin}>login</button>
-      <button onClick={handleSignup}>sign up</button>
-    </div>
+    <>
+      <Form.Control className="my-2" onChange={handleChange} name="username" type="text" placeholder="username..." />
+      <Form.Control className="my-2" onChange={handleChange} name="password" type="text" placeholder="password..." />
+      <Button className="mx-2" onClick={handleLogin} variant="success">
+        login
+      </Button>
+      <Button className="mx-2" onClick={handleSignup}>
+        sign up
+      </Button>
+      <div className="d-flex my-2">
+        <Form.Control
+          ref={inputPhoneNum}
+          name="phoneNum"
+          type="number"
+          placeholder="phone num"
+          className="my-2"
+          style={{ flex: 0.3, marginRight: "10px" }}
+        />
+        <Button className="m-2" onClick={handle인증번호받기}>
+          인증번호 생성
+        </Button>
+        <Form.Control
+          ref={inputVerificationCode}
+          name="verificationCode"
+          type="number"
+          className="my-2"
+          style={{ flex: 0.3, marginRight: "10px" }}
+        />
+        <Button className="m-2" onClick={handle인증번호확인}>
+          인증번호 확인
+        </Button>
+      </div>
+      {/* ### real test ###  */}
+      <div className="d-flex my-2">
+        <Form.Control
+          ref={inputPhoneNum}
+          name="phoneNum"
+          type="number"
+          placeholder="phone num"
+          className="my-2"
+          style={{ flex: 0.3, marginRight: "10px" }}
+        />
+        <Button className="m-2" onClick={handle인증번호받기2}>
+          handle인증번호받기2
+        </Button>
+        <Form.Control
+          ref={inputVerificationCode}
+          name="verificationCode"
+          type="text"
+          className="my-2"
+          style={{ flex: 0.3, marginRight: "10px" }}
+        />
+        <Button className="m-2" onClick={handle인증번호확인2}>
+          handle인증번호확인2
+        </Button>
+      </div>
+    </>
   );
 }
 
