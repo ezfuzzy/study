a38fb1bae066228e27681381a7a29bf95a66b717
ezfuzzy <mentor787@naver.com>
2024-08-23 11:35:30 +0900

    feat: gallery delete

--- react_test/base/src/pages/GalleryDetail.js
+++ react_test/base/src/pages/GalleryDetail.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Button, Card } from "react-bootstrap";
 import { Pagination } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import ConfirmModal from "../components/ConfirmModal";
 
 function GalleryDetail(props) {
   const { num } = useParams();
+  const navigate = useNavigate();
+
   const userName = useSelector((state) => state.userName);
 
   const [info, setInfo] = useState(null);
+  const [confirmShow, setConfirmShow] = useState(false);
 
   useEffect(() => {
     axios
@@ -22,11 +26,33 @@ function GalleryDetail(props) {
       });
   }, [num]);
 
+  const handleDelete = () => {
+    setConfirmShow(true);
+  };
+
+  const handleYes = () => {
+    // 삭제 내용
+    axios
+      .delete(`/gallery/${num}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
+
+    setConfirmShow(false);
+    navigate("/gallery");
+  };
+
+  const handleNo = () => {
+    setConfirmShow(false);
+  };
+
   return (
     <>
       <h1>Gallery Detail page</h1>
       {info && (
         <>
+          <ConfirmModal show={confirmShow} message="삭제하시겠습니까?" yes={handleYes} no={handleNo} />
           <Pagination>
             <Pagination.Item as={Link} to={`/gallery/${info.prevNum}`} disabled={info.prevNum === 0}>
               &larr; Prev
@@ -35,7 +61,7 @@ function GalleryDetail(props) {
               Next &rarr;
             </Pagination.Item>
             {userName === info.writer && (
-              <Button className="mx-5" variant="danger">
+              <Button onClick={handleDelete} className="mx-5" variant="danger">
                 Delete
               </Button>
             )}
