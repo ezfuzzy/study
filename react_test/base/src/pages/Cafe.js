c5a083df2ce8eef0915f42734fc5da34988e6403
ezfuzzy <ezfuzzy062@gmail.com>
2024-08-29 10:44:30 +0900

    add: cafe pagination

--- react_test/base/src/pages/Cafe.js
+++ react_test/base/src/pages/Cafe.js
@@ -1,52 +1,84 @@
+// src/pages/Cafe.js
+
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Button, Pagination, Table } from "react-bootstrap";
-import { Link, useSearchParams } from "react-router-dom";
+import { Pagination, Table } from "react-bootstrap";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 
-function range(start, end) {
-  const nums = [];
-  for (let i = start; i <= end; i++) {
-    nums.push(i);
-  }
-  return nums;
-}
-
-function Cafe(props) {
+function Cafe() {
+  //카페 글 목록 페이지 정보
   const [pageInfo, setPageInfo] = useState({
     list: [],
   });
-  const [pageArray, setPageArray] = useState([]);
-  const [searchState, setSearchState] = useState({});
+
+  //검색 조건과 키워드를 상태값으로 관리
+  const [searchState, setSearchState] = useState({
+    condition: "",
+    keyword: "",
+  });
+
+  // "/cafes?pageNum=x" 에서 pageNum 을 추출하기 위한 Hook
   const [params, setParams] = useSearchParams({ pageNum: 1 });
+  //페이징 숫자를 출력할때 사용하는 배열을 상태값으로 관리 하자
+  const [pageArray, setPageArray] = useState([]);
 
+  //글 목록 데이터 새로 읽어오는 함수
   const refresh = (pageNum) => {
+    //검색 기능과 관련된 query 문자열 읽어오기
     const query = new URLSearchParams(searchState).toString();
-    console.log(`query : ${query}`);
-
+    //axios 를 이용해서 pageNum 에 해당하는 데이터를 받아온다
     axios
       .get(`/api/cafes?pageNum=${pageNum}&${query}`)
       .then((res) => {
         console.log(res.data);
-        //API 서버로 부터 받아온 데이터를 state 로 관리 하기
+        //서버로 부터 응답된 데이터를 state 로 넣어준다
         setPageInfo(res.data);
-        //startPageNum, endPageNum 을 이용해서 pageArray 만들기 (state변경)
-        setPageArray(range(res.data.startPageNum, res.data.endPageNum));
+        //페이징 처리에 관련된 배열을 만들어서 state 로 넣어준다.
+        const result = range(res.data.startPageNum, res.data.endPageNum);
+        setPageArray(result);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  /*
+        useEffect() 안에 전달한 함수는
+        1. Cafe 컴포넌트가 활성화 되는 시점에 1번 호출된다.
+        2. params 가 변경이 될때마다 호출된다 
+    */
   useEffect(() => {
-    const pageNum = params.get("pageNum");
+    //query 파라미터 값을 읽어와 본다
+    let pageNum = params.get("pageNum");
+    //만일 존재 하지 않는다면 1 페이지로 설정
+    if (pageNum == null) pageNum = 1;
+    //해당 페이지의 내용을 원격지 서버로 부터 받아온다
     refresh(pageNum);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [params]);
+
+  //페이지를 이동할 hook
+  const navigate = useNavigate();
+
+  //페이징 UI 를 만들때 사용할 배열을 리턴해주는 함수
+  function range(start, end) {
+    const result = [];
+    for (let i = start; i <= end; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+
+  // navigate() 함수를 이용해서 페이지를 변경하는 함수
+  const move = (pageNum = 1) => {
+    //검색조건에 맞는 query 문자열을 얻어내기
+    const query = new URLSearchParams(searchState).toString();
+    navigate(`/cafe?pageNum=${pageNum}&${query}`);
+  };
 
   return (
     <>
-      <h1>카페</h1>
-      <Button className="my-3" variant="success" as={Link} to="/cafe/new">
-        새글 작성
-      </Button>
+      <Link to="/api/cafes/new">새글 작성</Link>
+      <h1>Cafe 글 목록 입니다</h1>
       <Table striped bordered size="sm">
         <thead>
           <tr>
@@ -70,32 +102,15 @@ function Cafe(props) {
         </tbody>
       </Table>
       <Pagination className="mt-3">
-        <Pagination.Item
-          onClick={() => {
-            //setParams() 함수를 이용해서 search parameter 값을 변경
-            setParams({ pageNum: pageInfo.startPageNum - 1 });
-          }}
-          disabled={pageInfo.startPageNum === 1}>
+        <Pagination.Item onClick={() => move(pageInfo.startPageNum - 1)} disabled={pageInfo.startPageNum === 1}>
           &laquo;
         </Pagination.Item>
-        {pageArray.map((num) => (
-          <Pagination.Item
-            onClick={() => {
-              //setParams() 함수를 이용해서 search parameter 값을 변경한다
-              setParams({ pageNum: num });
-            }}
-            key={num}
-            active={num === pageInfo.pageNum}>
-            {num}
+        {pageArray.map((item) => (
+          <Pagination.Item onClick={() => move(item)} key={item} active={pageInfo.pageNum === item}>
+            {item}
           </Pagination.Item>
         ))}
-        <Pagination.Item
-          onClick={() => {
-            setParams({ pageNum: pageInfo.endPageNum + 1 });
-          }}
-          disabled={pageInfo.endPageNum === pageInfo.totalPageCount}>
-          &raquo;
-        </Pagination.Item>
+        <Pagination.Item disabled={pageInfo.endPageNum >= pageInfo.totalPageCount}>&raquo;</Pagination.Item>
       </Pagination>
     </>
   );
