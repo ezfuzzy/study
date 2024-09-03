57985d0183cd1f323f867609b4844efe8b96c510
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-03 14:41:48 +0900

    add: kt - gallery

--- KotlinStudy/step07gallery/src/main/java/com/example/step07gallery/GalleryAdapter.kt
+++ KotlinStudy/step07gallery/src/main/java/com/example/step07gallery/GalleryAdapter.kt
@@ -5,12 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 class GalleryAdapter(val context: Context, val layoutRes:Int, val list:List<GalleryDto>) : BaseAdapter() {
 
     val inflater: LayoutInflater = LayoutInflater.from(context)
-    var index = 0
 
     override fun getCount(): Int {
         return list.size
@@ -24,26 +25,33 @@ class GalleryAdapter(val context: Context, val layoutRes:Int, val list:List<Gall
         return list[idx].num.toLong()
     }
 
-    override fun getView(idx: Int, view: View?, viewGroup: ViewGroup?): View {
+    override fun getView(idx: Int, view: View?, parent: ViewGroup?): View {
 
-        var convertView = view
+        var convertView = view // view가 nullable이니까 convertView도 nullable
 
         // 재활용하는 구조임 view가 100개가 아니라
 
         if(view == null) {
-            convertView = inflater.inflate(layoutRes, viewGroup, false)
-            println(index++)
+            convertView = inflater.inflate(layoutRes, parent, false)
         }
         val dto = list[idx]
-        val textWriter: TextView? = convertView?.findViewById(R.id.writer)
-        val textCaption: TextView? = convertView?.findViewById(R.id.caption)
-        val textRegdate: TextView? = convertView?.findViewById(R.id.regdate)
+        // !! - null이 아니라는 단언
+        val imageView: ImageView = convertView!!.findViewById(R.id.imageView)
+        val textWriter: TextView = convertView.findViewById(R.id.writer)
+        val textCaption: TextView = convertView.findViewById(R.id.caption)
+        val textRegdate: TextView = convertView.findViewById(R.id.regdate)
 
-        textWriter?.text = dto.writer
-        textCaption?.text = dto.caption
-        textRegdate?.text = dto.regdate
+        textWriter.text = dto.writer
+        textCaption.text = dto.caption
+        textRegdate.text = dto.regdate
 
-        return convertView!! // !! - null이 아니라는 단언
+        Glide.with(context)
+            .load(dto.imagePath)
+            .centerCrop()
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(imageView)
+
+        return convertView
     }
     
 
--- KotlinStudy/step07gallery/src/main/java/com/example/step07gallery/MainActivity.kt
+++ KotlinStudy/step07gallery/src/main/java/com/example/step07gallery/MainActivity.kt
@@ -1,15 +1,56 @@
 package com.example.step07gallery
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val list = mutableListOf<GalleryDto>()
+    lateinit var adapter: GalleryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        adapter = GalleryAdapter(this, R.layout.listview_cell, list)
+        val listView: ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
+
+        refresh()
+    }
+
+    fun refresh() {
+
+        // coroutine - thread보다 가볍게 동작하는
+        lifecycleScope.launch(Dispatchers.Main) {
+            // get data
+            val result = RestApiClient.get("http://192.168.0.205:8888/gallery?pageNum=1")
+
+            // list . add
+            val jsonObj = JSONObject(result)
+            // "list" 키에 있는 배열 추출
+            val arr = jsonObj.getJSONArray("list")
+
+            for (i in 0 until arr.length()){
+                val cur = arr.getJSONObject(i)
+                val dto = GalleryDto().apply {
+                    num = cur.getInt("num")
+                    writer = cur.getString("writer")
+                    caption = cur.getString("caption")
+                    regdate = cur.getString("regdate")
+                    imagePath = "http://192.168.0.205:8888/upload/images/${cur.getString("saveFileName")}"
+                }
+
+                list.add(dto)
+            }
+
+            // ui update
+            adapter.notifyDataSetChanged()
+        }
     }
 }
