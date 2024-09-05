b4d82a784b7c6e9bd4ac72fa0d4418a6ed93c226
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-05 13:13:41 +0900

    add: app - fragment, customView

---
+++ KotlinStudy/step08fragment/src/main/java/com/example/step08fragment/SubActivity.java
@@ -0,0 +1,16 @@
+package com.example.step08fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sub);
+
+    }
+}
