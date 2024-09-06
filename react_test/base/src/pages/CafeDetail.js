549d96c3b2247275b714a2eaaa75d6369d745820
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-06 11:45:29 +0900

    add: cafe detail

--- react_test/base/src/pages/CafeDetail.js
+++ react_test/base/src/pages/CafeDetail.js
@@ -4,7 +4,7 @@ import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom"
 
 import myCss from "./css/cafe_detail.module.css";
 import binder from "classnames/bind";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 import ConfirmModal from "../components/ConfirmModal";
 const cx = binder.bind(myCss);
@@ -14,6 +14,8 @@ function CafeDetail(props) {
   const navigate = useNavigate();
   const userName = useSelector((state) => state.userName);
   const [params, setParams] = useSearchParams();
+  const dispatch = useDispatch();
+
   const [state, setState] = useState({});
   const [confirmShow, setConfirmShow] = useState(false);
 
@@ -47,6 +49,26 @@ function CafeDetail(props) {
 
   const handleNo = () => {
     setConfirmShow(false);
+  };
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+
+    if (!userName) {
+      const payload = { show: true, message: "로그인한 사용자만 댓글을 작성할 수 있습니다." };
+      dispatch({ type: "LOGIN_MODAL", payload });
+      return;
+    }
+
+    const action = e.target.action;
+    const method = e.target.method;
+    const formData = new FormData(e.target);
+
+    //const json = Object.fromEntries(formData.entries)
+
+    axios[method](action, formData)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -104,7 +126,11 @@ function CafeDetail(props) {
       </nav>
       <ConfirmModal show={confirmShow} message="삭제하시겠습니까?" yes={handleYes} no={handleNo} />
       <h3>댓글</h3>
-      <form action={`/api/cafes/${num}/comments`} method="post" className={cx("comment-form")}>
+      <form
+        action={`/api/cafes/${num}/comments`}
+        method="post"
+        className={cx("comment-form")}
+        onSubmit={handleCommentSubmit}>
         <input type="hidden" name="ref_group" defaultValue={state.num} />
         <input type="hidden" name="target_id" defaultValue={state.writer} />
         <textarea name="content"></textarea>
