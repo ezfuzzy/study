7ecd38258198f44ccf595085c502469b71575bf6
ezfuzzy <ezfuzzy062@gmail.com>
2024-09-30 10:10:28 +0900

    fix: 로그아웃 후에 아무것도 입력하지 않아도 로그인버튼을 누르면 로그인되는 현상 수정

--- react_test/base/src/App.js
+++ react_test/base/src/App.js
@@ -1,16 +1,16 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-import { useOutlet } from "react-router-dom";
-import BsNavBar from "./components/BsNavBar";
-import { useSelector } from "react-redux";
-import LoginModal from "./components/LoginModal";
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { useOutlet } from "react-router-dom"
+import BsNavBar from "./components/BsNavBar"
+import { useSelector } from "react-redux"
+import LoginModal from "./components/LoginModal"
 
 function App() {
-  const currentOutlet = useOutlet();
+  const currentOutlet = useOutlet()
 
   //로그인 모달과 관련된 값을 redux store 로 부터 읽어온다.
-  const loginModal = useSelector((state) => state.loginModal);
+  const loginModal = useSelector((state) => state.loginModal)
 
   return (
     <>
@@ -18,9 +18,9 @@ function App() {
       <Container>
         <div>{currentOutlet}</div>
       </Container>
-      <LoginModal show={loginModal.show} message={loginModal.message} url={loginModal.url} />
+      {loginModal.show && <LoginModal show={loginModal.show} message={loginModal.message} url={loginModal.url} />}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
